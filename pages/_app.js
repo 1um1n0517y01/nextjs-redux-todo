2f8e92f3c6e97d883018f5f9d819a43cfc541f8b
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-05-09 21:00:51 +0200

    Persisting State with redux-persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { Provider } from 'react-redux';
-import { store } from '../redux/store';
+import { store, persistor } from '../redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import TodoList from '../components/TodoList';
 
-export default function Index() {
+export default function Home() {
   return (
     <h1 className='flex flex-col items-center justify-center min-h-screen'>
       <TodoList />
